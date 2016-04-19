a3fcab4f89576ca0e7c561bb7ec04b2074b4c318
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-04-19 18:43:21 -0500

    add participants new edit update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419204803) do
+ActiveRecord::Schema.define(version: 20160419223743) do
+
+  create_table "participants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "gender"
+    t.string   "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "studies", force: :cascade do |t|
     t.string   "title"
